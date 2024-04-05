b87581f116450ba09db08c82c9530b6c19755ded
Melak <km250542@ncratleos.com>
2024-04-05 23:33:47 +0200

    Deployment pt.I

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: '',
+
+    
+    firebase: {
+        apiKey: "AIzaSyCf_PrGwXwdxG3ypBt6ZYVKvG5Fk_HBwyU",
+        authDomain: "wmc-demo-00.firebaseapp.com",
+        projectId: "wmc-demo-00",
+        storageBucket: "wmc-demo-00.appspot.com",
+        messagingSenderId: "398954994925",
+        appId: "1:398954994925:web:edb95fae508530239700e4",
+        measurementId: "G-PC48REPSTK"
+    }
+};
